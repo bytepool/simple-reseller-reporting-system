ffd3a9a39ef86372b81ef8a7289f23731f6f21cf
Aljoscha Lautenbach <aljoscha.lautenbach@gmail.com>
2020-02-11 22:11:22 +0100

    Add config dir with example configuration.

---
+++ config/products.py
@@ -0,0 +1,11 @@
+# Configuration file for product information
+
+no_commission = ["Product A", "Product B"]
+
+product_categories = ["Category Foo"]
+    
+own_category = "Own"
+
+# variants
+foo_category = "Foo"
+foo = ["Foo Variant A", "Foo Variant B"]
